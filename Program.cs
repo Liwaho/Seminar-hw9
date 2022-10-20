897c41272875c7a2265a268a18a4bdea143587bd
Liwaho Xu <lwhollow@yandex.ru>
2022-10-20 18:19:41 +0500

    seond task

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
 
-Console.WriteLine("Input N: ");
+/*Console.WriteLine("Input N: ");
 int n = int.Parse(Console.ReadLine());
 int a = 1;
 
@@ -22,11 +22,23 @@ int Natural (int n, int a)
             Console.Write(Natural(n, a+1) + ", ");
         }
         return a;
-}
+}*/
 
 /*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30*/
+
+Console.WriteLine("Input N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Input M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Сумма элементов от "+ n +" до " + m + ": " + Sum(n, m));
+
+int Sum(int n, int m)
+{
+    if (n==m) return m;
+    return m + Sum(n,m-1);
+}
 
 /*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 m = 2, n = 3 -> A(m,n) = 9
