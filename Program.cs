4135d4ffff8803d181232e2f2ba777028578c7fd
Liwaho Xu <lwhollow@yandex.ru>
2022-10-20 18:30:13 +0500

    third task

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ int Natural (int n, int a)
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30*/
 
-Console.WriteLine("Input N: ");
+/*Console.WriteLine("Input N: ");
 int n = int.Parse(Console.ReadLine());
 Console.WriteLine("Input M: ");
 int m = int.Parse(Console.ReadLine());
@@ -39,7 +39,22 @@ int Sum(int n, int m)
     if (n==m) return m;
     return m + Sum(n,m-1);
 }
+*/
 
 /*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29*/
+
+Console.WriteLine("Input N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Input M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Функция Аккермана: " + Akk(n, m));
+
+int Akk(int n, int m)
+{
+    if (n == 0) return m + 1;
+                if (n != 0 & m == 0) return Akk(n - 1, 1);
+                if (n > 0 & m > 0) return Akk(n - 1, Akk(n, m - 1));
+                return Akk(n,m);
+}
